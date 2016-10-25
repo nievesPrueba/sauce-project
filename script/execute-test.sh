31123ff5bf610eefa7d4b92e80ab6eea977d9409
nievesSopra <mnievescaracena@gmail.com>
2016-10-25 15:49:39 +0200

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -10,6 +10,6 @@ REGEX_TAG="(.*)-TESTME"
   mv surefire-reports prueba01;
   git status
 #  git remote add test-output https://nievesSopra:24ca3b0580115685d9e5bc31034c410d397dbae3@github.com/nievesSopra/hello-world.git; 
-  #git remote add test-output https://24ca3b0580115685d9e5bc31034c410d397dbae3@github.com/nievesSopra/hello-world.git; 
+  git remote add test-output https://a74bae55fcb0706910ed7cb5c9d9310935b1c7ad@github.com/nievesSopra/hello-world.git; 
   #git push test-output prueba01; 
 #fi
