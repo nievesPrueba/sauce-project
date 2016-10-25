876c4ad8463dbab005ed67fb0b5e9157230be0ea
nievesSopra <mnievescaracena@gmail.com>
2016-10-25 13:58:39 +0200

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -8,6 +8,6 @@ if [[ "${TRAVIS_TAG}" =~ ${REGEX_TAG} ]]; then
   mvn test; 
   cd /home/travis/build/nievesSopra/sauce-project/target; 
   mv surefire-reports prueba01; 
-  git remote add test-output ssh://nievesSopra@github.com/hello-world.git; 
+  git remote add test-output https://nievesSopra:da3c9ff133a3f5ce0c0546100e4df518ec7dfeeb@github.com/nievesSopra/hello-world.git; 
   git push test-output prueba01; 
 fi
