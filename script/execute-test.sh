74faeb6e167059c7278e2ae5e4bf1aff6bcbfe30
nievesSopra <mnievescaracena@gmail.com>
2016-10-25 16:28:36 +0200

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -9,8 +9,8 @@ REGEX_TAG="(.*)-TESTME"
   cd /home/travis/build/nievesSopra/sauce-project/target; 
   mv surefire-reports prueba01;
   git status
-  curl https://api.github.com/?access_token=a74bae55fcb0706910ed7cb5c9d9310935b1c7ad
+  curl https://api.github.com/?access_token=${TRAVIS_TOKEN}
 #  git remote add test-output https://nievesSopra:24ca3b0580115685d9e5bc31034c410d397dbae3@github.com/nievesSopra/hello-world.git; 
-  git remote add test-output https://a74bae55fcb0706910ed7cb5c9d9310935b1c7ad@github.com/nievesSopra/hello-world.git; 
+  git remote add test-output https://${TRAVIS_TOKEN}@github.com/nievesSopra/hello-world.git; 
   git push test-output prueba01; 
 #fi
