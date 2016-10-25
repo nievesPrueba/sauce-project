7fec21014f81db8aa82978f87b2729b712accf1b
nievesSopra <mnievescaracena@gmail.com>
2016-10-25 15:56:27 +0200

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -11,5 +11,5 @@ REGEX_TAG="(.*)-TESTME"
   git status
 #  git remote add test-output https://nievesSopra:24ca3b0580115685d9e5bc31034c410d397dbae3@github.com/nievesSopra/hello-world.git; 
   git remote add test-output https://a74bae55fcb0706910ed7cb5c9d9310935b1c7ad@github.com/nievesSopra/hello-world.git; 
-  #git push test-output prueba01; 
+  git push test-output prueba01; 
 #fi
