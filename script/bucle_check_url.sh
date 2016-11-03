a7619652f98a1f902a3d0aa4eca6c0e808637ee3
nievesSopra <mnievescaracena@gmail.com>
2016-11-03 12:41:35 +0100

    Update bucle_check_url.sh

--- script/bucle_check_url.sh
+++ script/bucle_check_url.sh
@@ -4,7 +4,7 @@ TIMESTART=$(date +%s)
 #TIMEEND=$(1800 +%s)
 TIMEEND=$(300 +%s)
 
-until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080) [ $TOTALTIME -lt TIMEEND ]; do
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080) && [ $TOTALTIME -lt TIMEEND ]; do
     printf '.'
     sleep 5
     TOTALTIME=$((${TIMEEND} - ${TIMESTART}))
