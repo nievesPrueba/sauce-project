51802122d6048a974fa21f9d46b6edfb4793682c
nievesSopra <mnievescaracena@gmail.com>
2016-11-03 11:44:08 +0100

    Stop loop at TIMEEND

--- script/bucle_check_url.sh
+++ script/bucle_check_url.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+TIMESTART=$(date +%s)
+#TIMEEND=$(1800 +%s)
+TIMEEND=$(300 +%s)
+
 until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080); do
+    TOTALTIME=$((${TIMEEND} - ${TIMESTART}))
     printf '.'
     sleep 5
 done
