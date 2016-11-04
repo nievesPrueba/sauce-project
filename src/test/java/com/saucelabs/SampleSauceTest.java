5ea6d7486a790f2caac5de42cc38e093982b5ab8
nievesSopra <mnievescaracena@gmail.com>
2016-11-04 11:46:44 +0100

    Update SampleSauceTest.java

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -98,11 +98,12 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
     @Test(dataProvider = "hardCodedBrowsers")
     public void webDriver(String browser, String version, String os) throws Exception {
         WebDriver driver = createDriver(browser, version, os);
-//        driver.get("http://www.amazon.com/");
+        driver.get("http://www.amazon.com/");
+        assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
         // la url travis - docker http://127.0.0.1:8088/extranet-ssff
-        driver.get("http://192.168.99.100:8088/extranet-ssff");
-//        assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
-        assertEquals(driver.getTitle(), "Portal Clientes Grupo ASV Servicios Funerarios");
+       // driver.get("http://192.168.99.100:8088/extranet-ssff");
+       // assertEquals(driver.getTitle(), "Portal Clientes Grupo ASV Servicios Funerarios");
+        
         driver.quit();
     }
 
