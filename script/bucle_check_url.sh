d64118254fdad3e4b9e00cb35db1a072fe176953
nievesSopra <mnievescaracena@gmail.com>
2016-11-04 11:28:12 +0100

    Update bucle_check_url.sh

--- script/bucle_check_url.sh
+++ script/bucle_check_url.sh
@@ -3,7 +3,7 @@
 TIMESTART=$(date +%s)
 TIMEEND=$(date +%s)
 
-MAX_SECS=500
+MAX_SECS=600
 
 until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8088/extranet-ssff) || [ "$TOTALTIME" -gt "$MAX_SECS" ]; do
     printf '.'
@@ -16,6 +16,6 @@ done
 
 if [ "$TOTALTIME" -gt "$MAX_SECS" ]; then
    docker-compose stop
-   echo "[ERROR] - Cannot to do docker-compose up"
+   echo "[ERROR] - Cannot to load url"
    exit 1
 fi
