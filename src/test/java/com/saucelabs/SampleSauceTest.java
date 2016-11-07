28af63df93ed4141e13afabbd692f8f306578991
nievesSopra <mnievescaracena@gmail.com>
2016-11-07 17:54:01 +0100

    cambiado el puerto
    @ondemand.saucelabs.com:4443/wd/hub 
    antes era puerto 80

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -56,7 +56,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
     public static Object[][] sauceBrowserDataProvider(Method testMethod) {
         return new Object[][]{
                 new Object[]{"internet explorer", "11", "Windows 8.1"},
-                new Object[]{"safari", "6", "OSX 10.8"},
+                //new Object[]{"safari", "6", "OSX 10.8"},
         };
     }
 
@@ -82,7 +82,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider, SauceOnD
         capabilities.setCapability(CapabilityType.PLATFORM, os);
         capabilities.setCapability("name", "Sauce Sample Test");
         webDriver.set(new RemoteWebDriver(
-                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:4443/wd/hub"),
                 capabilities));
         sessionId.set(((RemoteWebDriver) getWebDriver()).getSessionId().toString());
         return webDriver.get();
