e387ff36ae614b53ad23fc7e01bbc26ece95eea2
nievesSopra <mnievescaracena@gmail.com>
2016-12-07 10:41:51 +0100

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -14,10 +14,10 @@ if [[ "${TRAVIS_TAG}" =~ ${REGEX_TAG} ]]; then
 
   docker login -e="$DOCKERHUB_EMAIL" -u="$DOCKERHUB_USER" -p="$DOCKERHUB_PASSWORD";
   chmod +x ./script/bucle_check_url.sh;
-  docker-compose up -d;#nuevo
-  sudo fuser -k 3306/tcp;#nuevo
-  docker-compose stop;#nuevo
-  docker-compose -f rm;#nuevo
-  docker-compose up;#nuevo
+  #docker-compose up -d;#nuevo
+  #sudo fuser -k 3306/tcp;#nuevo
+  #docker-compose stop;#nuevo
+  #docker-compose -f rm;#nuevo
+  #docker-compose up;#nuevo
   docker-compose up -d & ./script/bucle_check_url.sh;
 fi
